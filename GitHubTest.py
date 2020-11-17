d713ef444d6dbd8459e82c90a2e7ecadd7f5b3f7
Jelzbieciak <71610230+Jelzbieciak@users.noreply.github.com>
2020-11-17 19:23:21 +0100

    Update 2 to SubBranch1
    Update 2 to SubBranch1

--- GitHubTest.py
+++ GitHubTest.py
@@ -1,7 +1,9 @@
-def print_me_something(str2):
+def print_me_something(str3):
 
-    print(str2)
+    print(str3)
     
 new_str = "Kubaa"
+new_str2 = "Anna"
 
 print_me_something(new_str)
+print_me_something(new_str2)
